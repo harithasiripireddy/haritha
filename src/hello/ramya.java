9a3f213470e00ed99ecbea584200fb41db910eec
harithasiripireddy <harithasiripireddy@gmail.com>
2019-11-26 12:01:17 +0530

    hai

---
+++ src/hello/ramya.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class ramya {
+
+	public static void main(String[] args) {
+		int i=10;
+		int j=20;
+		 int c=i+j;
+		
+	System.out.println("c"+c);
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
