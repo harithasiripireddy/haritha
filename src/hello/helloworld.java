cd5667010029ae3b96e7f1aed3eee4c88a28922e
harithasiripireddy <harithasiripireddy@gmail.com>
2019-11-26 11:20:31 +0530

    hello

---
+++ src/hello/helloworld.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class helloworld {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("welcome to sunshine");
+		
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
